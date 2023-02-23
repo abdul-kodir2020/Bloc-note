c8e0744440b49c7eeae3b2faa332869f0d5bd129
abdul-kodir2020 <62393812+abdul-kodir2020@users.noreply.github.com>
2023-02-23 02:18:31 +0100

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { useState } from 'react';
 import './style.css';
 import BlocNote from './BlocNote';
+import Header from './Header';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <BlocNote />;
+  return (
+    <>
+      <Header />
+      <div>
+        <Routes>
+          <Route path="/bloc-note" element={<BlocNote />} />
+        </Routes>
+      </div>
+    </>
+  );
 }
 
 export default App;
